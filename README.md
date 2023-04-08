08b926d2c2a41d36e1a73c8538089139594bc368
JD470 <julien.dumesges@laposte.net>
2023-04-08 19:14:57 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Quantifier
+
+This project is a small tool to see the size of your project
+
+Usage: project_quantifier [file formats you want to look(no binary files like .exe)]
+
+Here is how it works: <br>
+
+![Alt text](assets/screenshot.png)
---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::{env, fs};
+use ansi_term::Color;
+use walkdir::WalkDir;
+
+const VALUE: Color = Color::RGB(0, 175, 255);
+const IN_PARENTHESIS: Color = Color::RGB(150, 150, 150);
+const WHITE: Color = Color::RGB(255, 255, 255);
+
+/// Get lines of code
+fn get_loc(files: Vec<String>) -> usize{
+    let mut lines = 0;
+    for i in files{
+        let file = fs::read_to_string(i);
+        lines += file.unwrap().split('\n').collect::<Vec<&str>>().len();
+    }
+    lines
+}
+
+/// Returns the number with its size name
+fn format_size(number: usize) -> String{
+    let size_names: Vec<String> = vec!["KB", "MB", "GB"].into_iter().map(|e| e.to_string()).collect();
+    let mut biggest_name = 0;
+    for i in 1..4{
+        if number > 10usize.pow(i*3){
+            biggest_name = i-1;
+        }
+    }
+    return format!("{:.2}{}", number as f64/10.0f64.powf((biggest_name as f64 +1.0)*3.0), size_names[biggest_name as usize].clone());
+}
+
+/// Get the number of characters in all the files
+fn get_size(files: Vec<String>) -> String{
+    let mut size = 0;
+    for i in files{
+        size +=  fs::read_to_string(i).unwrap().len();
+    }
+    return format_size(size)
+}
+
+fn get_nb_of_files(files: Vec<String>, formats: Vec<String>) -> Vec<u32>{
+    formats.into_iter().map(|format|{
+        files.clone().into_iter().filter(|f| f.ends_with(format.as_str())).collect::<Vec<String>>().len() as u32
+    }).collect()
+}
+
+fn print_info(files: Vec<String>, formats: Vec<String>){
+    let lines = get_loc(files.clone());
+    let size = get_size(files.clone());
+    let nb_of_files = get_nb_of_files(files.clone(), formats.clone());
+
+    println!("{}", WHITE.bold().paint("[Project Quantifier]"));
+    println!("Lines of code: {}", VALUE.paint(format!("{lines}")));
+    println!("Code size: {}", VALUE.paint(format!("{size}")));
+
+    println!();
+
+    formats.iter().enumerate().map(|(index, format)| {
+        let formats_files_size = get_size(files.clone().iter().filter(|f| f.ends_with(format)).map(|f| f.to_string()).collect::<Vec<String>>());
+        println!("Files {format}: {} {}", 
+            VALUE.paint(format!("{}", nb_of_files[index])),
+            IN_PARENTHESIS.paint(format!("[{}]", formats_files_size))
+        );
+    }).for_each(drop);
+}
+
+fn main() {
+    cfg_if::cfg_if! {
+		if #[cfg(target_os = "windows")] {
+			output_vt100::init();
+		}
+	}
+
+    let args: Vec<String> = env::args().collect();
+    let formats: Vec<String> = args.iter().enumerate().filter(|(i, _)| {
+        *i >= 1
+    }).map(|(_, e)| {
+        e.to_string()
+    }).collect();
+    
+    let files: Vec<String> = WalkDir::new(".").into_iter().filter(|f| {
+            let name = f.as_ref().unwrap().path().to_str().unwrap();
+            for format in &formats{
+                if name.ends_with(format.as_str()){
+                    return true;
+                }
+            }
+            false
+        }
+    ).map(|e| {
+        e.unwrap().path().to_str().unwrap().to_string()
+    }).collect();
+
+    print_info(files, formats);
+}
